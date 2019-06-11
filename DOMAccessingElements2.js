33fdf4abb3a8f8d22887c8f92d6d16fe187ea2fb
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-06-11 16:19:07 -0500

    Update DOMAccessingElements2.js

---
+++ DOMAccessingElements2.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var x = document.querySelectorAll(".example");
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.backgroundColor = "lightgreen";
+    }
+}
