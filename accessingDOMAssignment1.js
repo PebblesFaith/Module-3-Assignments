cbcabe1b6054862802aee7bd2b907013aea965df
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-06-13 16:38:00 -0500

    Create accessingDOMAssignment1.js

---
+++ accessingDOMAssignment1.js
@@ -0,0 +1,15 @@
+
+/*
+function accessingDOM () {
+    var headings = document.getElementsByClassName('h1');
+    document.getElementsByClassName('h1') = headings[0].innerHTML;
+
+}
+*/
+
+function accessingDOM1 () {
+    var headings1 = document.querySelectorAll('.h1');
+    document.querySelectorAll('.h1') = headings1[0].innerHTML;
+
+}
+
