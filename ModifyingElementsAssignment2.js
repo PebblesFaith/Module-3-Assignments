f5d8c12df73fe262ae95c3228335e842a08976ac
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-06-14 11:52:21 -0500

    Update ModifyingElementsAssignment2.js

--- ModifyingElementsAssignment2.js
+++ ModifyingElementsAssignment2.js
@@ -1,3 +1,4 @@
-
+ /* Create JavaScript document object element to modify the hyperlink so that the hyperlink instead points to your
+    favorite website. */
 
 document.getElementById("myFavoritePage").setAttribute("href", "http://www.accounetrics.com/");
