b7d319dd379e122f8915a7b5782addf2db1e1b25
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-06-14 16:54:46 -0500

    Create creatingHTMLElements1.js

---
+++ creatingHTMLElements1.js
@@ -0,0 +1,13 @@
+
+
+var newHeading = document.createElement("h2");
+newHeading.innerHTML = "Learning the relationship between DOM API document object nodes and HTML elements are fun to watch my JavaScript language coding transfomations onto the HTML webpage.";
+document.getElementById("p1").append(newHeading);
+
+
+var newText = document.createTextNode("Hi, I'm new here");
+document.getElementById("p2").append(newText);
+
+var originalList= document.getElementById("myList2").lastChild;
+var exactCopy = originalList.cloneNode(true);
+document.getElementById("myList1").appendChild(exactCopy);
