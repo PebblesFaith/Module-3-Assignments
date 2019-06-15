6dc5bfbff400dcbb95e86dfb98b8ec8842650d10
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-06-15 18:05:07 -0500

    Update accessingDOMAssignment1.js

--- accessingDOMAssignment1.js
+++ accessingDOMAssignment1.js
@@ -1,15 +1,14 @@
 
-/*
+
 function accessingDOM () {
     var headings = document.getElementsByClassName('h1');
     document.getElementsByClassName('h1') = headings[0].innerHTML;
 
 }
-*/
 
-function accessingDOM1 () {
+/*function accessingDOM1 () {
     var headings = document.querySelectorAll('.h1');
-    document.querySelectorAll('.h1') = headings1[0].innerHTML;
+    document.querySelectorAll('.h1') = headings[0].innerHTML;
 
 }
-
+*/
